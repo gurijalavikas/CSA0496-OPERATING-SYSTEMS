5045c5c39f619ad14f5026295e0ff67b0690f626
gurijalavikas <124266420+gurijalavikas@users.noreply.github.com>
2024-03-04 13:32:04 +0530

    Add files via upload

---
+++ 17.the deadlock avoidance concept by simulating Banker’s algorithm with C.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+#define N 5
+#define M 3
+
+
+int isSafe(int processes[], int available[], int max[][M], int allocation[][M]) {
+  
+}
+
+int main() {
+    
+    if (isSafe(processes, available, max, allocation)) {
+        
+        printf("Resources allocated safely.\n");
+    } else {
+        printf("Resource allocation would lead to a deadlock. Waiting...\n");
+    }
+    
+    return 0;
+}
